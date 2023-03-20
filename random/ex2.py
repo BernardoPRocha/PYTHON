9d32262b0161ca1e94abff80eee891f7748f49e1
BernardoPRocha <RM99209@fiap.com.br>
2023-03-20 16:49:32 -0300

    terceiro commit

--- random/ex2.py
+++ random/ex2.py
@@ -1,16 +1,34 @@
-print("Digite a altura de 5 girafas")
-g1 = float(input())
-g2 = float(input())
-g3 = float(input())
-g4 = float(input())
-g5 = float(input())
-if g1 >= g2 and g1 >= g3 and g1 >= g4 and g1 >= g5 :
-    print("A girafa 1 é a mais alta")
-elif g2 >= g1 and g2 >= g3 and g2 >= g4 and g2 >= g5 :
-    print("A girafa 2 é a mais alta")
-elif g3 >= g2 and g3 >= g1 and g3 >= g4 and g3 >= g5 :
-    print("A girafa 3 é a mais alta")
-elif g4 >= g2 and g4 >= g3 and g4 >= g1 and g4 >= g5 :
-    print("A girafa 4 é a mais alta")
-elif g5 >= g2 and g5 >= g3 and g5 >= g4 and g5 >= g1 :
-    print("A girafa 5 é a mais alta")
+from random import randint 
+itens = ("Pedra", "Papel", "Tesoura")
+computador = randint(0, 2)
+print("Suas opções: ( 0 ) Pedra ( 1 ) Papel ( 2 ) Tesoura ")
+jogador = int(input("Qual a sua jogada? "))
+print("-=" * 11)
+print("Computador jogou {}" .format(itens[computador]))
+print("Jogador jogou {}" .format(itens[jogador]))
+print("-=" * 11)
+
+if computador == 0:
+    if jogador == 0:
+        print("Empate")
+    elif jogador == 1:
+        print("Jogador venceu")
+    elif jogador == 2:
+        print("Computador venceu")
+
+elif computador == 1:
+    if jogador == 0:
+        print("Computador venceu")
+    elif jogador == 1:
+        print("Empate")
+    elif jogador == 2:
+        print("Jogador venceu")
+
+elif computador == 2:
+    if jogador == 0:   
+        print("Jogador venceu")
+    elif jogador == 1:
+        print("Computador venceu")
+    elif jogador == 2:  
+        print("Empate")
+    
---
+++ variaveis/variavel2.py
@@ -0,0 +1,16 @@
+print("Digite a altura de 5 girafas")
+g1 = float(input())
+g2 = float(input())
+g3 = float(input())
+g4 = float(input())
+g5 = float(input())
+if g1 >= g2 and g1 >= g3 and g1 >= g4 and g1 >= g5 :
+    print("A girafa 1 é a mais alta")
+elif g2 >= g1 and g2 >= g3 and g2 >= g4 and g2 >= g5 :
+    print("A girafa 2 é a mais alta")
+elif g3 >= g2 and g3 >= g1 and g3 >= g4 and g3 >= g5 :
+    print("A girafa 3 é a mais alta")
+elif g4 >= g2 and g4 >= g3 and g4 >= g1 and g4 >= g5 :
+    print("A girafa 4 é a mais alta")
+elif g5 >= g2 and g5 >= g3 and g5 >= g4 and g5 >= g1 :
+    print("A girafa 5 é a mais alta")
