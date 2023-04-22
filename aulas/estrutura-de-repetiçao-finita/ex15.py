bf1f22225a7d2ac89338ee9f9751dcf4e5c1ed35
BernardoPRocha <RM99209@fiap.com.br>
2023-04-22 04:43:47 -0300

    exercicios de casa

--- aulas/estrutura-de-repetiçao-finita/ex15.py
+++ aulas/estrutura-de-repetiçao-finita/ex15.py
@@ -1,12 +1,28 @@
-preço = 0
-valorPago = 0
-troco = 0
-print("Digite o valor da compra")
-preço = float(input())
-print("Digite o valor pago")
-valorPago = float(input())
-troco = valorPago - preço
-if preço == valorPago:
-    print("O valor da compra é igual ao valor pago")
+valor_a_pagar = float(input("Digite o valor a ser pago: "))
+valor_pago = float(input("Digite o valor efetivamente pago: "))
+
+troco = valor_pago - valor_a_pagar
+print(troco)
+
+if troco < 0:
+    print("Valor pago é menor que o valor a ser pago!")
 else:
-    print(f"O troco é de R${troco}")
+    notas50 = int(troco / 50)
+    troco = troco % 50
+
+    notas20 = int(troco / 20)
+    troco = troco % 20
+
+    notas10 = int(troco / 10)
+    troco = troco % 10
+
+    notas5 = int(troco / 5)
+    troco = troco % 5
+
+    moedas1 = int(troco)
+
+    print(f"Notas de 50 reais: {notas50}")
+    print(f"Notas de 20 reais: {notas20}")
+    print(f"Notas de 10 reais: {notas10}")
+    print(f"Notas de 5 reais: {notas5}")
+    print(f"Moedas de 1 real: {moedas1}")
--- aulas/estrutura-de-repetiçao-finita/ex16.py
+++ aulas/estrutura-de-repetiçao-finita/ex16.py
@@ -1,23 +1,38 @@
-I = 0
-A = 0
-B = 0
-C = 0 
-print("Digite o valor de I")
-I = int(input())
-print("Digite o valor de A")
-A = float(input())
-print("Digite o valor de B")
-B = float(input())
-print("Digite o valor de C")
-C = float(input())
-lista = (A, B , C)
-crescente = sorted(lista)
-decrescente = sorted(lista, reverse=True)
-meio = (max)
-match I:
-    case 1:
-        print(crescente)
-    case 2:
-        print(decrescente)
-    case 3:
-        print(meio)
+i = int(input("Digite o valor de I: "))
+a = float(input("Digite o valor de A: "))
+b = float(input("Digite o valor de B: "))
+c = float(input("Digite o valor de C: "))
+
+print(f"Valores lidos: I={i}, A={a}, B={b}, C={c}")
+
+if i == 1:
+    # ordem crescente
+    lista = [a, b, c]
+    lista.sort()
+    print(f"Ordem crescente: {lista}")
+elif i == 2:
+    # ordem decrescente
+    lista = [a, b, c]
+    lista.sort(reverse=True)
+    print(f"Ordem decrescente: {lista}")
+elif i == 3:
+    # maior valor entre os outros dois
+    if a > b and a > c:
+        if b > c:
+            print(f"Ordem: {b}, {a}, {c}")
+        else:
+            print(f"Ordem: {c}, {a}, {b}")
+    elif b > a and b > c:
+        if a > c:
+            print(f"Ordem: {c}, {b}, {a}")
+        else:
+            print(f"Ordem: {a}, {b}, {c}")
+    elif c > a and c > b:
+        if a > b:
+            print(f"Ordem: {b}, {c}, {a}")
+        else:
+            print(f"Ordem: {a}, {c}, {b}")
+else:
+    print("Valor de I inválido.")
+
+    
